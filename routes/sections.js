8d004bc161906eba8fbc5a450b9ed4b361dfa50e
Azzarash <ricoazzarash@gmail.com>
2015-06-16 11:18:17 +0300

    Some new functions

--- routes/sections.js
+++ routes/sections.js
@@ -1,4 +1,35 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'admin',
+    password: 'Vbhdfhrhfanf1',
+    database: 'appdb'
+});
+
 /* GET sections page. */
 exports.display = function (req, res) {
-    res.render('sections');
+    var transfer = {
+        index: 0,
+        names: []
+    }
+
+    pool.getConnection(function (err, connection) {
+        query = connection.query('SELECT * from articles ORDER BY article_date DESC, article_id DESC');
+        query.on('error', function (err) {
+            throw err;
+        });
+        query.on('result', function (row) {
+            //console.log('The solution is: ', row);
+            transfer.names[transfer.index] = row.article_name;
+            transfer.index++;
+        });
+        query.on('end', function (result) {
+            console.log("OK");
+            console.log(transfer.names);
+            res.render('sections', {
+                index: transfer.index,
+                names: transfer.names
+            });
+        });
+    });
 };
